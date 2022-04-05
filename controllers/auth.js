9af9495aaaae9939ec4ecce7a7c2e5fc408da121
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-05 18:08:12 -0300

    Login of User

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,42 +7,68 @@ const createUser = async (req, res = response) => {
 
   try {
     let user = await User.findOne({ email: email });
-        if (user) {
-        return res.status(400).json({
-            ok: false,
-            msg: "Email exist in our register",
-        });
-        }
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Email exist in our register",
+      });
+    }
 
     user = new User(req.body);
-    
+
     //Encrypt of password
     const salt = bcrypt.genSaltSync();
-    user.password = bcrypt.hashSync( password, salt );
+    user.password = bcrypt.hashSync(password, salt);
 
     await user.save();
 
     res.status(201).json({
-        ok: true,
-        uid: user.id,
-        name: user.name,
+      ok: true,
+      uid: user.id,
+      name: user.name,
     });
   } catch (error) {
-        res.status(500).json({
-        ok: false,
-        msg: "Please contact the admin ...",
-        });
-    }
+    res.status(500).json({
+      ok: false,
+      msg: "Please contact the admin ...",
+    });
+  }
 };
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
   const { email, password } = req.body;
-    res.status(201).json({
-        ok: true,
-        msg: "login",
-        email,
-        password,
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "User does not exist with thath email or password",
+      });
+    }
+
+    // Confirmar los passwords
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password wrong",
+      });
+    }
+
+    res.json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
     });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Please contact the admin ...",
+    });
+  }
 };
 
 const revalidateToken = (req, res = response) => {
