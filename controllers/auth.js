8841d91b97b127f6a2248c9a85cddda0807debc4
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-05 16:47:44 -0300

    Create de User en DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,37 @@
-const {response} = require('express');
+const { response } = require("express");
+const User = require("../models/User");
 
-const createUser = (req, res = response) => {
-    const {name, email, password} = req.body; 
-        res.status(201).json({
-            ok: true,
-            msg: 'registro',
-            name,
-            email,
-            password
-        })
-    }
+const createUser = async (req, res = response) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({
+      ok: true,
+      msg: "registro",
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Please contact the admin ...",
+    });
+  }
+};
 
 const loginUser = (req, res = response) => {
-    const { email, password } = req.body; 
-        res.status(201).json({
-            ok: true,
-            msg: 'login',
-            email,
-            password
-        })
-    }
+  const { email, password } = req.body;
+  res.status(201).json({
+    ok: true,
+    msg: "login",
+    email,
+    password,
+  });
+};
 
 const revalidateToken = (req, res = response) => {
-        res.json({
-            ok: true,
-            msg: 'revalidate'
-        })
-    }
+  res.json({
+    ok: true,
+    msg: "revalidate",
+  });
+};
 
-module.exports = {createUser, loginUser, revalidateToken}
+module.exports = { createUser, loginUser, revalidateToken };
